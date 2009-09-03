9bd7b0f20debe1b2f3c8e95294193566bc97bd7a
macournoyer <macournoyer@gmail.com>
2009-09-03 09:26:42 -0400

    Initial work on livetyping

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,52 +1,54 @@
-function Chat() {
-  function scrollToBottom() {
-    window.scrollTo(0, document.body.clientHeight);
-  };
-  
-  $(document).ready(function() {
-    scrollToBottom();
+$(document).ready(function() {
+  Chat.scrollToBottom();
 
-    $("#msgbox").
-      keydown(function(event) {
-        if (event.which == 13) {
-          $("#msgForm").trigger("submit")[0].reset();
-          return false;
-        }
-      }).
-      focus();
-    
-    $("#msgForm").submitWithAjax(function() {
-      $("#msgForm")[0].reset();
-      $("#msgForm")[0].focus();
+  $("#msgbox").
+    keydown(function(e) {
+      if (e.which == 13) {
+        Chat.send(this.value);
+        return false;
+      }
+    }).
+    keyup(function(e) {
+      if (e.which == 32) { // space
+        Chat.send(this.value);
+      } else {
+        Chat.sendLater(this.value);
+      }
+    }).
+    focus();
+});
+
+var Chat = {
+  init: function() {
+    // setup ajax sender that empties the queue
+  },
+  
+  sendLater: function(data, cond) {
+    if (this.sendTimeout) clearTimeout(this.sendTimeout);
+    this.sendTimeout = setTimeout(function() {
+      Chat.send(data);
+    }, 400);
+  },
+  
+  send: function(data) {
+    if (this.sendTimeout) clearTimeout(this.sendTimeout);
+    this.enqueue(data);
+  },
+  
+  enqueue: function(data) {
+    this.queuedData = data;
+    this.dequeue();
+  }.
+  
+  dequeue: function() {
+    var data = this.queuedData;
+    this.queuedData = null;
+    $.post($("msgForm")[0].action, {message: data}, functon() {
+      if (Chat.queuedData) Chat.dequeue();
     });
-  });
-  
-  return {
-    log: function(id, html) {
-      var events = $("#log").find(".event");
-      for (var i = events.length - 1; i >= 0; i--) {
-        var message = events[i];
-        var messageId = parseInt(message.id.match(/message_(\d+)/)[1]);
-        // message already there
-        if (messageId == id) return;
-        // message is older, insert before
-        if (messageId < id) {
-          $(message).after(html);
-          scrollToBottom();
-          return;
-        };
-      };
-      // should never get here, but just in case...
-      $("#log").append(html);
-      scrollToBottom();
-    },
-    
-    join: function(userId, html) {
-      if ($("#user_" + userId).length > 0) $("#users").append(html);
-    },
+  },
 
-    leave: function(userId, html) {
-      $("#user_" + userId).remove();
-    },
+  scrollToBottom: function() {
+    window.scrollTo(0, document.body.clientHeight);
   }
-}
+};
