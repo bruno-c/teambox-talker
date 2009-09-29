3b4bd44a74d3505799e03181a47fae7e45a42392
macournoyer <macournoyer@gmail.com>
2009-09-29 19:51:44 -0400

    Send ping only when inactive.

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -134,13 +134,17 @@ function TalkerClient(options) {
           break;
       } 
     }
+    
     function onRawDataReceived(data) {
       console.info("TalkerClient::received raw: " + data);
     }
     
-    self.pingInterval = setInterval(function(){
-      self.ping();
-    }, 20000);
+    self.resetPing = function() {
+      if (self.pingInterval) clearInterval(self.pingInterval);
+      self.pingInterval = setInterval(function(){
+        self.ping();
+      }, 20000);
+    }
     
     self.join = function(line){
       if ($("ul#users li:contains('" + line.user + "')").length < 1){
@@ -150,6 +154,7 @@ function TalkerClient(options) {
     };
     
     self.ping = function(){
+      console.info("ping");
       protocol.send(JSON.encode({type: 'ping'}));
     }
     
@@ -157,6 +162,7 @@ function TalkerClient(options) {
     self.sendData = function(message) {
       // TODO encode to UTF8?
       protocol.send(JSON.encode(message));
+      self.resetPing();
     };
 
     self.connect = function(domain, port, room, user, token) {
@@ -164,6 +170,7 @@ function TalkerClient(options) {
         protocol.onopen = function() {
           // Connect to Talker server
           self.sendData({type: "connect", room: room, user: user, token: token});
+          self.resetPing();
         }
         // XXX even though we are connecting to onclose, this never gets fired
         //     after we shutdown orbited.
@@ -191,121 +198,3 @@ function TalkerClient(options) {
       self.sendData(message);
     };
 }
-
-// $(function() {
-//   Chat.scrollToBottom();
-//   Chat.log = $("#log");
-//   Chat.newMessageElement = $("#message");
-//   Chat.newMessageUrl = $("#msgForm").attr("action");
-//   Chat.newMessage();
-//   
-//   $("#msgbox").
-//     keydown(function(e) {
-//       if (e.which == 13) {
-//         Chat.send(this.value);
-//         Chat.newMessage();
-//         return false;
-//       }
-//     }).
-//     keyup(function(e) {
-//       if (e.which == 32) { // space
-//         Chat.send(this.value);
-//       } else {
-//         Chat.sendLater(this.value);
-//       }
-//     }).
-//     focus();
-// });
-// 
-// var Chat = {
-//   messages: {},
-//   currentMessage: null,
-//   
-//   sendLater: function(data) {
-//     if (data === "") return;
-//     if (this.sendTimeout) clearTimeout(this.sendTimeout);
-//     this.sendTimeout = setTimeout(function() {
-//       Chat.send(data);
-//     }, 400);
-//   },
-//   
-//   send: function(data) {
-//     if (data === "") return;
-//     if (this.sendTimeout) clearTimeout(this.sendTimeout);
-//     this.enqueue(data);
-//   },
-//   
-//   newMessage: function() {
-//     if (this.currentMessage) this.currentMessage.createElement().insertBefore(this.newMessageElement);
-//     this.currentMessage = new Message(currentUser.email);
-//     this.messages[this.currentMessage.uuid] = this.currentMessage;
-//     
-//     // Move the new message form to the bottom
-//     this.newMessageElement.
-//       appendTo(Chat.log).
-//       find("form").reset().
-//       find("textarea").focus();
-//     
-//     this.scrollToBottom();
-//   },
-//   
-//   receive: function(frame) {
-//     console.info(frame.body);
-//     var data = JSON.decode(frame.body);
-//     var message = this.messages[data.uuid];
-//     if (!message) {
-//       message = this.messages[data.uuid] = new Message(data.from, data.uuid);
-//       message.createElement();
-//     }
-// 
-//     message.update(data.content);
-//     
-//     if (this.currentMessage.content == null) {
-//       this.newMessageElement.
-//         appendTo(Chat.log).
-//         find("textarea").focus();
-//     }
-// 
-//     this.scrollToBottom();
-//   },
-//   
-//   enqueue: function(data) {
-//     this.currentMessage.content = data;
-//     this.dequeue();
-//   },
-//   
-//   dequeue: function() {
-//     var message = this.currentMessage;
-//     $.post(this.newMessageUrl, { uuid: message.uuid, content: message.content });
-//   },
-// 
-//   scrollToBottom: function() {
-//     window.scrollTo(0, document.body.clientHeight);
-//   }
-// };
-// 
-// function Message(from, uuid) {
-//   this.from = from;
-//   this.uuid = uuid || Math.uuid();
-//   this.elementId = "message-" + this.uuid;
-// }
-// 
-// Message.prototype.update = function(content) {
-//   this.content = content;
-//   if (this.element) this.element.find(".content").html(content);
-// }
-// 
-// Message.prototype.createElement = function() {
-//   // Create of find the message HTML element
-//   this.element = Chat.log.find("#" + this.elementId);
-//   if (this.element.length == 0) {
-//     this.element = $("<tr/>").
-//       addClass("event").
-//       addClass("message").
-//       attr("id", this.elementId).
-//       append($("<td/>").addClass("author").html(this.from)).
-//       append($("<td/>").addClass("content").html(this.content || "")).
-//       appendTo(Chat.log);
-//   }
-//   return this.element;
-// }
