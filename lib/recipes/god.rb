4a8d64918e819589aa245ba49613cef059abb24f
macournoyer <macournoyer@gmail.com>
2009-10-04 00:20:37 -0400

    Fix god cap recipe.

--- lib/recipes/god.rb
+++ lib/recipes/god.rb
@@ -1,9 +1,13 @@
 namespace :god do
   task :start, :roles => :app do
-    sudo "god -c #{current_path}/config/thin.god", :as => 'root'
+    sudo "god -c #{current_path}/config/thin.god"
+  end
+
+  task :status, :roles => :app do
+    sudo "god status"
   end
   
   task :stop, :roles => :app do
-    sudo "god quit", :as => 'root'
+    sudo "god quit"
   end
 end
