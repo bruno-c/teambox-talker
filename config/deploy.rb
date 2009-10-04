1becb3899cc7e3b879e3d0af2abcb1c952a80eea
macournoyer <macournoyer@gmail.com>
2009-10-03 23:45:59 -0400

    Remove condig/database.yml from git (generated by chef on server). Couple updates to chef recipes... almost there...

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,16 +1,16 @@
 set :application, "talker"
 set :repository,  "git@github.com:macournoyer/talker.git"
-set :location,    "ec2-67-202-35-82.compute-1.amazonaws.com"
 
 set :deploy_to, "/mnt/apps/#{application}"
 set :deploy_via, :remote_cache
 
-set :runner, "app"
+set :runner, "admin"
 set :user, "admin"
 # set :port, 30000 # ssh port
 
 set :scm, :git
 
-role :app, location
-role :web, location
-role :db,  location, :primary => true
+server = "ec2-174-129-187-160.compute-1.amazonaws.com"
+role :app, server
+role :web, server
+role :db,  server, :primary => true
