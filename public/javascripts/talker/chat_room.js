9c71c10dd8d1260455b6e42844972b2556551b36
Gary Haran <gary.haran@gmail.com>
2009-10-09 22:47:28 -0400

    Oh and if I wasn't belligerent enough I'm also fucking with your vimeo.  Bite me!

--- public/javascripts/talker/chat_room.js
+++ public/javascripts/talker/chat_room.js
@@ -71,6 +71,7 @@ var ChatRoom = {
   formatMessage: function(content, noScroll) {
     var image_expression    = /(^https?:\/\/[^\s]+\.(?:gif|png|jpeg|jpg)$)/gi;
     var youtube_expression  = /^(?:http\S+youtube\.com\/watch\?v=)([\w-]+)(?:\S*)$/;
+    var vimeo_expression    = /^(?:http\S+vimeo\.com\/)(\d+)/;
     var url_expression      = /[^\s]+\.[^\s]+/gim;
     var protocol_expression = /^(http|https|ftp|ftps|ssh|irc|mms|file|about|mailto|xmpp):\/\//;
     
@@ -93,6 +94,19 @@ var ChatRoom = {
           + '  width="425" height="355" '
           + '  allowfullscreen="true"></embed>'
           + '</object>');
+      });
+    } else if (content.match(vimeo_expression)){
+      return content.replace(vimeo_expression, function(locator){
+        return locator.replace(vimeo_expression, '<object width="400" height="220">'
+          + '<param name="allowfullscreen" value="true" />'
+          + '<param name="allowscriptaccess" value="always" />'
+          + '<param name="movie" value="http://vimeo.com/moogaloop.swf?clip_id=$1'
+          + '&amp;server=vimeo.com&amp;show_title=1&amp;show_byline=1&amp;show_portrait=0&amp;color=&amp;fullscreen=1" />'
+          + '<embed src="http://vimeo.com/moogaloop.swf?clip_id=$1&amp;server=vimeo.com&amp;show_title=1&amp;show_byline=1&amp;'
+          + 'show_portrait=0&amp;color=&amp;fullscreen=1" '
+          + 'type="application/x-shockwave-flash" allowfullscreen="true" allowscriptaccess="always" width="400" height="220">'
+          + '</embed></object>'
+        );
       });
     } else if (content.match(url_expression)) {
       return content.replace(url_expression, function(locator){
