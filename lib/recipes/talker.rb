1094887b1e4e9180b8c06734964706bd310d7ca1
macournoyer <macournoyer@gmail.com>
2009-10-12 23:15:15 -0400

    Better cap talks for god & talker

--- lib/recipes/talker.rb
+++ lib/recipes/talker.rb
@@ -1,18 +1,22 @@
-GOD_COMMANDS = [:monitor, :start, :stop, :restart]
+GOD_COMMANDS = %w( start stop restart )
+TALKER_SERVICES = %w( channel presence logger )
 
 namespace :talker do
-  GOD_COMMANDS.each do |command|
-    desc "Send #{command} command to Talker processes."
-    task command, :roles => :app do
-      sudo "god #{command} talker-server"
+  TALKER_SERVICES.each do |service|
+    namespace service do
+      GOD_COMMANDS.each do |command|
+        desc "Send #{command} command to Talker #{service} processes."
+        task command, :roles => :app do
+          sudo "god #{command} talker-#{service}"
+        end
+      end
     end
   end
-  
-  namespace :logger do
+  namespace :all do
     GOD_COMMANDS.each do |command|
-      desc "Send #{command} command to Talker Logger processes."
-      task command, :roles => :app do
-        sudo "god #{command} talker-logger"
+      desc "Send #{command} command to all Talker processes."
+      task command do
+        TALKER_SERVICES.each { |s| send(s).send(command) }
       end
     end
   end
