13763a710d9ec7fdf87951a4b23ef36e9a84a0cf
macournoyer <macournoyer@gmail.com>
2009-10-18 12:23:15 -0400

    Move thinking sphinx to a plugin.

--- config/environment.rb
+++ config/environment.rb
@@ -21,7 +21,6 @@ Rails::Initializer.run do |config|
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem "haml"
   config.gem "compass"
-  config.gem "thinking-sphinx", :lib => 'thinking_sphinx', :version => '1.2.12', :source => "http://gemcutter.org"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
