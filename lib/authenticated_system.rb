6586062e49748607592a08f77360d30420035d16
macournoyer <macournoyer@gmail.com>
2009-10-21 14:05:52 -0400

    Better access denied for json and xml formats

--- lib/authenticated_system.rb
+++ lib/authenticated_system.rb
@@ -68,13 +68,13 @@ module AuthenticatedSystem
           flash[:error] = "Access denied!"
           redirect_to login_url
         end
+        error = { :type => "error", :message => "Access denied" }
+        format.json { render :json => error }
+        format.xml { render :xml => error }
         # format.any doesn't work in rails version < http://dev.rubyonrails.org/changeset/8987
         # Add any other API formats here.  (Some browsers, notably IE6, send Accept: */* and trigger 
         # the 'format.any' block incorrectly. See http://bit.ly/ie6_borken or http://bit.ly/ie6_borken2
         # for a workaround.)
-        format.any(:json, :xml) do
-          request_http_basic_authentication 'Web Password'
-        end
       end
     end
 
