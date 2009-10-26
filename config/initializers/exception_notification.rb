88154e6b2dee901854284903c08e7c110aabf1f5
macournoyer <macournoyer@gmail.com>
2009-10-26 14:47:28 -0400

    Try another way to make ExceptionNotifier smtp settings special.

--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -1,12 +1,17 @@
 ExceptionNotifier.exception_recipients = %w(macournoyer@gmail.com gary.haran@gmail.com)
 ExceptionNotifier.sender_address = %("Rails Error" <notifier@talkerapp.com>)
 
-ExceptionNotifier.smtp_settings = {
-  :address => "smtp.gmail.com",
-  :port => "587",
-  :domain => "talkerapp.com",
-  :authentication => :plain,
-  :user_name => "notifier@talkerapp.com",
-  :password => "Cn8JChhsZsqSYX3TTRnX",
-  :enable_starttls_auto => true
-}
+# Override default ActionMailer smtp settings
+ExceptionNotifier.class_eval do
+  def smtp_settings
+    {
+      :address => "smtp.gmail.com",
+      :port => "587",
+      :domain => "talkerapp.com",
+      :authentication => :plain,
+      :user_name => "notifier@talkerapp.com",
+      :password => "Cn8JChhsZsqSYX3TTRnX",
+      :enable_starttls_auto => true
+    }
+  end
+end
