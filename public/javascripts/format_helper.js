8e8145f97ddd64b913f5ee80b4fb40dcc090319f
Gary Haran <gary.haran@gmail.com>
2009-10-28 23:29:52 -0400

    fixing ordinal issue.

--- public/javascripts/format_helper.js
+++ public/javascripts/format_helper.js
@@ -84,7 +84,7 @@ var FormatHelper = {
     var minutes = date.getMinutes() - date.getMinutes() % 5;
     
     return months[date.getMonth()] + '&nbsp;' 
-      + date.getDate().toOrdinal() + ',&nbsp;'
+      + new Number(date.getDate()).toOrdinal() + ',&nbsp;'
       + date.getFullYear();
   },
   
