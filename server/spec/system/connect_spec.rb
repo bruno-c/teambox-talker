f4cdc0265a01d77d694b67cb0136fe0901ac1eb2
macournoyer <macournoyer@gmail.com>
2009-10-30 09:39:26 -0400

    Put server mysql specs into another process to prevent hang.

--- server/spec/system/connect_spec.rb
+++ server/spec/system/connect_spec.rb
@@ -10,10 +10,18 @@ EM.describe "A user that connects to a room" do
     end
   end
 
-  xit "should close previous connection if user already connected" do
-    connect :user => {:id => 1}
+  it "should be connected if already connected" do
+    connect :user => {:id => 1} do |client|
+      client.on_error do |message|
+        fail message
+      end
+    end
     
     connect :user => {:id => 1} do |client|
+      client.on_error do |message|
+        fail message
+      end
+      
       client.on_connected do
         done
       end
