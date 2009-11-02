d6f04cf333160d58ae3de944fbb977238a53abd1
macournoyer <macournoyer@gmail.com>
2009-11-02 11:39:04 -0500

    Move new message logging to Logger.

--- app/javascripts/talker/logger.js
+++ app/javascripts/talker/logger.js
@@ -2,7 +2,40 @@ Talker.Logger = function() {
   var self = this;
   
   self.onMessageReceived = function(data) {
-    Receiver.push(data);
+    // format content appropriately
+    if (data.paste && data.paste != 'null'){
+      data.content = FormatHelper.formatPaste(data);
+    } else {
+      data.content = FormatHelper.text2html(data.content);
+    }
+    
+    var last_row    = $('#log tr:last');
+    var last_author = last_row.attr('author');
+    
+    
+    if (last_author == data.user.name && last_row.hasClass('message') && !last_row.hasClass('private') && !data.private){ // only append to existing blockquote group
+      last_row.find('blockquote')
+        .append($('<p/>').attr('time', data.time).html(data.content));
+    } else {
+      var element = $('<tr/>')
+        .attr('author', data.user.name)
+        .addClass('received')
+        .addClass('message')
+        .addClass('user_' + data.user.id)
+        .addClass('event')
+        .addClass(data.user.id == currentUser.id ? 'me' : '')
+        .addClass(data.private ? 'private' : '')
+          .append($('<td/>').addClass('author')
+            .append('\n' + data.user.name + '\n')
+            .append($('<img/>').attr('src', '/images/avatar_default.png').attr('alt', data.user.name).addClass('avatar'))
+            .append($('<b/>').addClass('blockquote_tail').html('<!-- display fix --->')))
+          .append($('<td/>').addClass('message')
+            .append($('<blockquote/>')
+              .append($('<p/>').attr('time', data.time)
+                .html(data.content))));
+      
+      element.appendTo('#log');
+    }
   }
 }
 
--- app/javascripts/talker/receiver.js
+++ app/javascripts/talker/receiver.js
@@ -60,51 +60,6 @@ Receiver = {
   
   idle: function(data, replay) {
     $("#user_" + data.user.id).css('opacity', 0.5).addClass('idle');
-  },
-  
-  message: function(data, replay, linkToLogs) {
-    // format content appropriately
-    if (data.paste && data.paste != 'null'){
-      data.content = FormatHelper.formatPaste(data);
-    } else {
-      data.content = FormatHelper.text2html(data.content);
-    }
-    
-    var last_row    = $('#log tr:last');
-    var last_author = last_row.attr('author');
-    
-    
-    if (linkToLogs){
-      var url = '/rooms/' + data.room + '/logs/' + FormatHelper.getUrlDate(data.time) + '#' + data.time;
-      var link_to_log = $('<a/>').attr('href', url).addClass('logs').html('view in logs');
-    } else {
-      var link_to_log = '';
-    }
-    
-    if (last_author == data.user.name && last_row.hasClass('message') && !last_row.hasClass('private') && !data.private){ // only append to existing blockquote group
-      last_row.find('blockquote')
-        .append($('<p/>').attr('time', data.time).html(data.content).append(link_to_log));
-    } else {
-      var element = $('<tr/>')
-        .attr('author', data.user.name)
-        .addClass('received')
-        .addClass('message')
-        .addClass('user_' + data.user.id)
-        .addClass('event')
-        .addClass(data.user.id == currentUser.id ? 'me' : '')
-        .addClass(linkToLogs ? 'logs' : '')
-        .addClass(data.private ? 'private' : '')
-          .append($('<td/>').addClass('author')
-            .append('\n' + data.user.name + '\n')
-            .append($('<img/>').attr('src', '/images/avatar_default.png').attr('alt', data.user.name).addClass('avatar'))
-            .append($('<b/>').addClass('blockquote_tail').html('<!-- display fix --->')))
-          .append($('<td/>').addClass('message')
-            .append($('<blockquote/>')
-              .append($('<p/>').attr('time', data.time)
-                .html(data.content).append(link_to_log))));
-      
-      element.appendTo('#log');
-    }
   }
 }
 
