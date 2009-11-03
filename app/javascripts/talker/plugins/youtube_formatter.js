b781753bae8357b894207840558bdc50b29c32dd
Gary Haran <garyharan@stephanie-dugass-macbook.local>
2009-11-03 16:24:22 -0500

    readded youtube.

--- app/javascripts/talker/plugins/youtube_formatter.js
+++ app/javascripts/talker/plugins/youtube_formatter.js
@@ -2,21 +2,21 @@ Talker.YoutubeFormatter = function() {
   var self = this;
   
   self.onFormatMessage = function(event){
-    // var youtube_expression = /^(?:http\S+youtube\.com\/watch\?v=)([\w-]+)(?:\S*)$/;
-    // var youtube_match  = event.content.match(youtube_expression);
-    // 
-    // if (youtube_match){
-    //   event.complete(
-    //     '<object width="425" height="355">'
-    //     + '<param name="movie" value="http://www.youtube.com/v/' + youtube_match[1] + '"></param>'
-    //     + '<param name="allowFullScreen" value="true"></param>'
-    //     + '<embed src="http://www.youtube.com/v/' + youtube_match[1] + '"'
-    //     + ' type="application/x-shockwave-flash"'
-    //     + '  width="425" height="355" '
-    //     + '  allowfullscreen="true"></embed>'
-    //     + '</object>'
-    //   );
-    //   return false;
-    // }
+    var youtube_expression = /^(?:http\S+youtube\.com\/watch\?v=)([\w-]+)(?:\S*)$/;
+    var youtube_match  = event.content.match(youtube_expression);
+    
+    if (youtube_match){
+      event.complete(
+        '<object width="425" height="355">'
+        + '<param name="movie" value="http://www.youtube.com/v/' + youtube_match[1] + '"></param>'
+        + '<param name="allowFullScreen" value="true"></param>'
+        + '<embed src="http://www.youtube.com/v/' + youtube_match[1] + '"'
+        + ' type="application/x-shockwave-flash"'
+        + '  width="425" height="355" '
+        + '  allowfullscreen="true"></embed>'
+        + '</object>'
+      );
+      return false;
+    }
   }
 };
