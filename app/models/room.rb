60f14b9b1a0b8b94f0f3436ea480784ae636b707
macournoyer <macournoyer@gmail.com>
2009-11-03 13:42:04 -0500

    Add helper method to install core plugins. Make chat log work.

--- app/models/room.rb
+++ app/models/room.rb
@@ -6,4 +6,8 @@ class Room < ActiveRecord::Base
   
   validates_presence_of :name
   validates_uniqueness_of :name, :scope => :account_id
+  
+  def to_json(options = {})
+    super(options.merge(:only => [:name, :id]))
+  end
 end
