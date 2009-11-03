823cf3b404d59c2710b24077acd03c0c5f467500
Gary Haran <garyharan@stephanie-dugass-macbook.local>
2009-11-03 16:14:40 -0500

    disabled youtube formatter.

--- app/javascripts/talker/formatter.js
+++ app/javascripts/talker/formatter.js
@@ -7,32 +7,7 @@ var FormatHelper = {
     var protocol_expression  = /^(http|https|ftp|ftps|ssh|irc|mms|file|about|mailto|xmpp):\/\//;
     var multiline_expression = /\n/gim;
 
-    var content = content.replace(/</g, '&lt;').replace(/>/g, '&gt;');
-        
-    if (content.match(vimeo_expression)){
-      return content.replace(vimeo_expression, function(locator){
-        return locator.replace(vimeo_expression, '<object width="400" height="220">'
-          + '<param name="allowfullscreen" value="true" />'
-          + '<param name="allowscriptaccess" value="always" />'
-          + '<param name="movie" value="http://vimeo.com/moogaloop.swf?clip_id=$1'
-          + '&amp;server=vimeo.com&amp;show_title=1&amp;show_byline=1&amp;show_portrait=0&amp;color=&amp;fullscreen=1" />'
-          + '<embed src="http://vimeo.com/moogaloop.swf?clip_id=$1&amp;server=vimeo.com&amp;show_title=1&amp;show_byline=1&amp;'
-          + 'show_portrait=0&amp;color=&amp;fullscreen=1" '
-          + 'type="application/x-shockwave-flash" allowfullscreen="true" allowscriptaccess="always" width="400" height="220">'
-          + '</embed></object>'
-        );
-      });
-    } else if (content.match(url_expression)) {
-      return content.replace(url_expression, function(locator){
-        return '<a href="' 
-          +  (!locator.match(protocol_expression) ? 'http://' : '') + locator
-          + '" target="_blank">' 
-          +   locator 
-          + "</a>";
-      });
-    } else {
-      return content;
-    }
+    return content.replace(/</g, '&lt;').replace(/>/g, '&gt;');
   },
   
   timestamp2date: function(timestamp){
