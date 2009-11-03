221f07ca14f5225b480af4e64772f3913e93a4c7
macournoyer <macournoyer@gmail.com>
2009-11-03 08:44:40 -0500

    Add note RE setting up Nginx for /avatar on prod

--- app/controllers/avatars_controller.rb
+++ app/controllers/avatars_controller.rb
@@ -5,7 +5,6 @@ class AvatarsController < ApplicationController
     hash = params[:id]
     size = params[:s] || 80
     # This is just for dev env, in prod this is done on Nginx level.
-    # TODO setup proxy_pass in Nginx in production
     redirect_to "http://www.gravatar.com/avatar/#{hash}.jpg?s=#{size}"
   end
 end
