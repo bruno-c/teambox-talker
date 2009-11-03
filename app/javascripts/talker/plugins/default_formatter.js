f99be17e7d07539c4d9b9c2e070f8533893d44cc
Gary Haran <garyharan@stephanie-dugass-macbook.local>
2009-11-03 16:06:45 -0500

    added urls to default formatter.

--- app/javascripts/talker/plugins/default_formatter.js
+++ app/javascripts/talker/plugins/default_formatter.js
@@ -3,11 +3,23 @@ Talker.DefaultFormatter = function() {
   var self = this;
   
   self.onFormatMessage = function(event){
-    console.info("Inside DefaultFormatter.onFormatMessage");
-    if (event.content.match(/\n/gim)){
-      event.complete('<div><pre>' + event.content.replace(/</g, '&lt;').replace(/>/g, '&gt;') + '</pre></div>');
+    var url_expression = /(https?:\/\/|www\.)[^\s<]*/gi;
+    var protocol_expression  = /^(http|https|ftp|ftps|ssh|irc|mms|file|about|mailto|xmpp):\/\//;
+    
+    var content = event.content.replace(/</g, '&lt;').replace(/>/g, '&gt;');
+    
+    if (content.match(url_expression)){
+      event.complete(content.replace(url_expression, function(locator){
+        return '<a href="' 
+          +  (!locator.match(protocol_expression) ? 'http://' : '') + locator
+          + '" target="_blank">' 
+          +   locator 
+          + "</a>";
+      }));
+    } else if (event.content.match(/\n/gim)){
+      event.complete('<div><pre>' + content + '</pre></div>');
     } else {
-      event.complete(event.content.replace(/</g, '&lt;').replace(/>/g, '&gt;'));
+      event.complete(content);
     }
   }
 };
