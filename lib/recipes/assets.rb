e22bdc736f1fa9fd7e2f8d056e644b173eda3106
macournoyer <macournoyer@gmail.com>
2009-11-03 16:24:01 -0500

    Reminify js on deploy.

--- lib/recipes/assets.rb
+++ lib/recipes/assets.rb
@@ -1,3 +1,3 @@
 after "deploy:symlink", :roles => :app do
-  run "cd #{release_path}; rake RAILS_ENV=production sprockets:install_script"
+  run "cd #{release_path}; rake RAILS_ENV=production jsmin"
 end
