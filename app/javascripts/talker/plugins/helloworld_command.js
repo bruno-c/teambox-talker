468653c789e505153860fe405c435959fe9b8fb3
Gary Haran <gary.haran@gmail.com>
2009-11-03 23:35:01 -0500

    added helloworld command.  Good for purpose of tutorials and demos.

---
+++ app/javascripts/talker/plugins/helloworld_command.js
@@ -0,0 +1,9 @@
+Talker.HelloWorldCommand = function() {
+  var self = this;
+  
+  self.onCommand = function(event) {
+    if (event.command == "hello") {
+      alert('Hello world!');
+    }
+  }
+}
