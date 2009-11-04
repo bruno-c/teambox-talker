52a042531d02a3fa3ffecc8b7ea72a9761720961
Gary Haran <gary.haran@gmail.com>
2009-11-04 18:55:13 -0500

    fixing notification for join/leaves.  Discovered a dissapointing bug in JS at the same time :-(

--- app/javascripts/talker/plugins/system_notification.js
+++ app/javascripts/talker/plugins/system_notification.js
@@ -3,6 +3,12 @@ Talker.SystemNotification = function() {
   
   self.onNotification = function(event) {
     if (window.notifications.notifications_support()) {
+      
+      if (event.type != 'message'){
+        var presence_messages = {'join': 'has joined the room', 'leave': 'has left the room'};
+        event.content = presence_messages[event.type];
+      } 
+      
       window.notifications.notify({
         title: Talker.room.name,
         description: event.user.name + ": " + event.content
