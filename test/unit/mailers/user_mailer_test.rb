5f79e9672b41971baf45610041e7db4c698ba4e2
macournoyer <macournoyer@gmail.com>
2009-11-04 13:42:07 -0500

    Fix From in mailer tests.

--- test/unit/mailers/user_mailer_test.rb
+++ test/unit/mailers/user_mailer_test.rb
@@ -3,14 +3,14 @@ require File.dirname(__FILE__) + "/../../test_helper"
 class UserMailerTest < ActionMailer::TestCase
   def test_invitation
     mail = UserMailer.create_invitation(users(:quentin), "http://test.talkerapp.com/", "invitee@example.com").encoded
-    assert_match "From: no-reply@talkerapp.com", mail
+    assert_match "From: Talker <no-reply@talkerapp.com>", mail
     assert_match "To: invitee@example.com", mail
     assert_match "http://test.talkerapp.com/", mail
   end
 
   def test_reset_password
     mail = UserMailer.create_reset_password("quentin@example.com", "http://test.talkerapp.com/").encoded
-    assert_match "From: no-reply@talkerapp.com", mail
+    assert_match "From: Talker <no-reply@talkerapp.com>", mail
     assert_match "To: quentin@example.com", mail
     assert_match "http://test.talkerapp.com/", mail
   end
