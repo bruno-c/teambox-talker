54bb68afea8d5c5a157d43c98b15a286a0899456
macournoyer <macournoyer@gmail.com>
2009-11-04 11:13:16 -0500

    Add root_url helper

--- app/helpers/url_helper.rb
+++ app/helpers/url_helper.rb
@@ -2,4 +2,8 @@ module UrlHelper
   def link_to_with_active(name, url, options = {})
     link_to name, url, options.merge(:class => [options[:class], ('active' if current_page?(url))].compact.join(' '))
   end
+  
+  def root_url
+    "#{request.protocol}://#{request.host}/"
+  end
 end
