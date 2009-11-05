1210a7b8c981cf215ef3e598ad6f31ee7bf823eb
macournoyer <macournoyer@gmail.com>
2009-11-05 10:27:49 -0500

    Fix user rename migration. Ran manually on server.

--- db/migrate/20091104164655_make_user_name_more_strict.rb
+++ db/migrate/20091104164655_make_user_name_more_strict.rb
@@ -1,6 +1,6 @@
 class MakeUserNameMoreStrict < ActiveRecord::Migration
   def self.up
-    User.all.each { |u| u.update_attribute :name, u.name.gsub(/\A[[:cntrl:]\\<>\/&\s]+\z/, "") }
+    User.all.each { |u| u.update_attribute :name, u.name.gsub(/[[:cntrl:]\\<>\/&\s]+/, "_") }
   end
 
   def self.down
