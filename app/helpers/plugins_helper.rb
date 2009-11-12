57e852cb37ad3e40a122d5a091ee8760b91d7483
Gary Haran <gary.haran@gmail.com>
2009-11-12 11:28:47 -0500

    ensure that plugin names are not part of the code.

--- app/helpers/plugins_helper.rb
+++ app/helpers/plugins_helper.rb
@@ -24,7 +24,17 @@ module PluginsHelper
   
   def install_plugins_source
     plugins = current_account.installed_plugins.map do |plugin|
-      "\n#{plugin.source}"
+      "
+Talker.Plugin_#{plugin.id} = function(){ // #{plugin.name} by #{plugin.author.name}
+  var plugin = this;
+  
+  try{
+    #{plugin.source}
+  } catch(e){
+    Talker.error(e, \"#{plugin.name}\");
+  }
+}
+"
     end
     
     plugins.join("\n")
@@ -33,7 +43,7 @@ module PluginsHelper
   # temporary... just for testing.
   def install_plugins
     plugins = current_account.installed_plugins.map do |plugin|
-      "Talker.plugins.push(new #{plugin.name}());"
+      "Talker.plugins.push(new Talker.Plugin_#{plugin.id}());"
     end
     
     plugins.join("\n")
--- db/migrate/20091110222809_create_plugins.rb
+++ db/migrate/20091110222809_create_plugins.rb
@@ -25,17 +25,13 @@ class CreatePlugins < ActiveRecord::Migration
     p.author_id = User.talker.id
     p.shared = true
     p.source = <<-eos
-      Talker.HelloCommand = function() {
-        var self = this;
-        
-        self.onCommand = function(event) {
-          if (event.command == "hello") {
-            alert('Hello world!');
-            $('#msgbox').val('');
-            return false;
-          }
-        }
-      }
+plugin.onCommand = function(event) {
+  if (event.command == "hello") {
+    alert('Hello world!');
+    $('#msgbox').val('');
+    return false;
+  }
+}
 eos
     p.save
     
@@ -46,26 +42,22 @@ eos
     p.author_id = User.talker.id
     p.shared = true
     p.source = <<-eos
-      Talker.DockBadge = function() {
-        var self = this;
+plugin.count = 0;
 
-        self.count = 0;
+plugin.onLoaded = function() {
+  plugin.onBlur = function() {
+    plugin.onMessageReceived = function(event) {
+      plugin.count = plugin.count + 1;
+      window.dockBadge(plugin.count);
+    }
+  }
 
-        self.onLoaded = function() {
-          self.onBlur = function() {
-            self.onMessageReceived = function(event) {
-              self.count = self.count + 1;
-              window.dockBadge(self.count);
-            }
-          }
-
-          self.onFocus = function() {
-            self.count = 0;
-            window.dockBadge('');
-            self.onMessageReceived = function(event) { }
-          }
-        }
-      }
+  plugin.onFocus = function() {
+    plugin.count = 0;
+    window.dockBadge('');
+    plugin.onMessageReceived = function(event) { }
+  }
+}
 eos
     p.save
     
@@ -75,23 +67,19 @@ eos
     p.author_id = User.talker.id
     p.shared = true
     p.source = <<-eos
-      Talker.NewMessageNotifications = function() {
-        var self = this;
+plugin.onLoaded = function() {
+  plugin.onBlur = function() {
+    plugin.onMessageReceived = function(event) {
+      Talker.notify(event);
+    }
+  }
 
-        self.onLoaded = function() {
-          self.onBlur = function() {
-            self.onMessageReceived = function(event) {
-              Talker.notify(event);
-            }
-          }
-
-          self.onFocus = function() {
-            self.onMessageReceived = function(event) { }
-          }
-        }
-      }
+  plugin.onFocus = function() {
+    plugin.onMessageReceived = function(event) { }
+  }
+}
 eos
-        p.save
+    p.save
 
     p = Plugin.new
     p.name = 'Talker.UserLeaveNotifications'
@@ -99,24 +87,20 @@ eos
     p.author_id = User.talker.id
     p.shared = true
     p.source = <<-eos
-      Talker.UserLeaveNotifications = function() {
-        var self = this;
+plugin.onLoaded = function() {
+  plugin.onBlur = function() {
+    plugin.onLeave = function(event) {
+      event.content = "has left the room.";
+      Talker.notify(event);
+    }
+  }
 
-        self.onLoaded = function() {
-          self.onBlur = function() {
-            self.onLeave = function(event) {
-              event.content = "has left the room.";
-              Talker.notify(event);
-            }
-          }
-
-          self.onFocus = function() {
-            self.onLeave = function() {  }
-          }
-        }
-      };
+  plugin.onFocus = function() {
+    plugin.onLeave = function() {  }
+  }
+}
 eos
-        p.save
+    p.save
 
     p = Plugin.new
     p.name = 'Talker.UserJoinNotifications'
@@ -124,53 +108,45 @@ eos
     p.author_id = User.talker.id
     p.shared = true
     p.source = <<-eos
-      Talker.UserJoinNotifications = function() {
-        var self = this;
+plugin.onLoaded = function() {
+  plugin.onBlur = function() {
+    plugin.onJoin = function(event) {
+      event.content = "has entered the room.";
+      Talker.notify(event);
+    }
+  }
 
-        self.onLoaded = function() {
-          self.onBlur = function() {
-            self.onJoin = function(event) {
-              event.content = "has entered the room.";
-              Talker.notify(event);
-            }
-          }
-
-          self.onFocus = function() {
-            self.onJoin = function() {  }
-          }
-        }
-      };
+  plugin.onFocus = function() {
+    plugin.onJoin = function() {  }
+  }
+}
 eos
-        p.save
+    p.save
     p = Plugin.new
     p.name = 'Talker.TitleMessageCount'
     p.description = "Updates the title with message count when Talker is behind other windows."
     p.author_id = User.talker.id
     p.shared = true
     p.source = <<-eos
-      Talker.TitleMessageCount = function() {
-        var self = this;
+plugin.count = 0;
+plugin.originalTitle = document.title;
 
-        self.count = 0;
-        self.originalTitle = document.title;
+plugin.onLoaded = function() {
+  plugin.onBlur = function() {
+    plugin.onMessageReceived = function(event) {
+      plugin.count = plugin.count + 1;
+      document.title = plugin.originalTitle + " (" + plugin.count + ")";;
+    }
+  }
 
-        self.onLoaded = function() {
-          self.onBlur = function() {
-            self.onMessageReceived = function(event) {
-              self.count = self.count + 1;
-              document.title = self.originalTitle + " (" + self.count + ")";;
-            }
-          }
-
-          self.onFocus = function() {
-            self.count = 0;
-            document.title = self.originalTitle;
-            self.onMessageReceived = function(event) { }
-          }
-        }
-      };
+  plugin.onFocus = function() {
+    plugin.count = 0;
+    document.title = plugin.originalTitle;
+    plugin.onMessageReceived = function(event) { }
+  }
+}
 eos
-        p.save
+    p.save
 
     p = Plugin.new
     p.name = 'Talker.TwitterStatuses'
@@ -178,31 +154,27 @@ eos
     p.author_id = User.talker.id
     p.shared = true
     p.source = <<-eos
-      Talker.TwitterStatuses = function() {
-        var self = this;
+plugin.onAfterMessageReceived = function(event){
+  var last_insertion = Talker.Logger.lastRow();
+  var twitter_status_expression = /https*:\\/\\/twitter.com\\/\\w+\\/status\\/(\\d+)/i;
+  var last_anchor = last_insertion.find('a');
+  var last_href = last_anchor.attr('href') || '';
 
-        self.onAfterMessageReceived = function(event){
-          var last_insertion = Talker.Logger.lastRow();
-          var twitter_status_expression = /https*:\\/\\/twitter.com\\/\\w+\\/status\\/(\\d+)/i;
-          var last_anchor = last_insertion.find('a');
-          var last_href = last_anchor.attr('href') || '';
+  if (last_href.match(twitter_status_expression)){
+    var id = last_href.match(twitter_status_expression)[1];
+    var url = 'http://twitter.com/statuses/show/' + id + '.json?callback=?';
 
-          if (last_href.match(twitter_status_expression)){
-            var id = last_href.match(twitter_status_expression)[1];
-            var url = 'http://twitter.com/statuses/show/' + id + '.json?callback=?';
-
-            $.getJSON(url, function(data){
-              last_insertion.find('a').html($('<q/>').css({
-                clear: 'both',
-                float: 'left'
-              }).append(
-                $('<img/>').attr({src: data.user.profile_image_url, height: 48, width: 48})
-                .css('margin', '4px')
-              ).append('"' + data.text + '"'));
-            });
-          }
-        }
-      };
+    $.getJSON(url, function(data){
+      last_insertion.find('a').html($('<q/>').css({
+        clear: 'both',
+        float: 'left'
+      }).append(
+        $('<img/>').attr({src: data.user.profile_image_url, height: 48, width: 48})
+        .css('margin', '4px')
+      ).append('"' + data.text + '"'));
+    });
+  }
+}
 eos
      p.save
    
@@ -212,20 +184,16 @@ eos
      p.author_id = User.talker.id
      p.shared = true
      p.source = <<-eos
-      Talker.UserLeave = function() {
-        var self = this;
+plugin.onLeave = function(event) {
+  var element = $('<tr/>').attr('author', h(event.user.name)).addClass('received').addClass('notice').addClass('user_' + event.user.id).addClass('event')
+    .append($('<td/>').addClass('author'))
+    .append($('<td/>').addClass('message')
+      .append($('<p/>').attr('time', event.time).html(h(event.user.name) + ' has left the room')));
 
-        self.onLeave = function(event) {
-          var element = $('<tr/>').attr('author', h(event.user.name)).addClass('received').addClass('notice').addClass('user_' + event.user.id).addClass('event')
-            .append($('<td/>').addClass('author'))
-            .append($('<td/>').addClass('message')
-              .append($('<p/>').attr('time', event.time).html(h(event.user.name) + ' has left the room')));
-
-          element.appendTo('#log');
-        }
-      }
+  element.appendTo('#log');
+}
 eos
-        p.save
+      p.save
 
       p = Plugin.new
       p.name = 'Talker.UserJoin'
@@ -233,18 +201,14 @@ eos
       p.author_id = User.talker.id
       p.shared = true
       p.source = <<-eos
-      Talker.UserJoin = function() {
-        var self = this;
+plugin.onJoin = function(event) {
+  var element = $('<tr/>').attr('author', h(event.user.name)).addClass('received').addClass('notice').addClass('user_' + event.user.id).addClass('event')
+    .append($('<td/>').addClass('author'))
+    .append($('<td/>').addClass('message')
+      .append($('<p/>').attr('time', event.time).html(h(event.user.name) + ' has entered the room')));
 
-        self.onJoin = function(event) {
-          var element = $('<tr/>').attr('author', h(event.user.name)).addClass('received').addClass('notice').addClass('user_' + event.user.id).addClass('event')
-            .append($('<td/>').addClass('author'))
-            .append($('<td/>').addClass('message')
-              .append($('<p/>').attr('time', event.time).html(h(event.user.name) + ' has entered the room')));
-
-          element.appendTo('#log');
-        }
-      } 
+  element.appendTo('#log');
+}
 eos
       p.save
       
@@ -254,57 +218,51 @@ eos
       p.author_id = User.talker.id
       p.shared = true
       p.source = <<-eos
-      Talker.EmoticonsFormatter = function() {
-        var self = this;
+plugin.Emoticon = function(matchers, path, meaning){
+  var self = this;
 
-        self.onAfterMessageReceived = function(event){
-          _.each(Talker.emoticons, function(emoticon){
-            var element = Talker.Logger.lastRow().find('p:last').get(0);
-            emoticon.findAndReplace(element);
-          });
-        }
-      };
+  self.strings = matchers ;
+  self.path = path;
+  self.meaning = meaning;
 
-      Talker.Emoticon = function(matchers, path, meaning){
-        var self = this;
+  self.findAndReplace = function(domElement){
+    _.each(self.strings, function(string){
+     $(domElement).findAndReplace(string, plugin.replacementString(string));
+    });
+  }
 
-        self.strings = matchers ;
-        self.path = path;
-        self.meaning = meaning;
+  self.replacementString = function(string){
+    return '<img src="' 
+        + self.path 
+        + '" class="emoticons" height="16" width="16" alt="' 
+        + string
+        + '" title="' 
+        + self.meaning + '"  />';
+  }
 
-        self.findAndReplace = function(domElement){
-          _.each(self.strings, function(string){
-           $(domElement).findAndReplace(string, self.replacementString(string));
-          });
-        }
+  return self;
+}
 
-        self.replacementString = function(string){
-          return '<img src="' 
-              + self.path 
-              + '" class="emoticons" height="16" width="16" alt="' 
-              + string
-              + '" title="' 
-              + self.meaning + '"  />';
-        }
+plugin.onAfterMessageReceived = function(event){
+  _.each(Talker.emoticons, function(emoticon){
+    var element = Talker.Logger.lastRow().find('p:last').get(0);
+    emoticon.findAndReplace(element);
+  });
+}
 
-        return self;
-      }
+plugin.emoticons = [];
+plugin.emoticons.push(new Talker.Emoticon(['>:-)', '>:)'],            "/images/icons/smiley-evil.png",    "evil"));
+plugin.emoticons.push(new Talker.Emoticon([':-))', ':))'],            "/images/icons/smiley-lol.png",     "laughing"));
+plugin.emoticons.push(new Talker.Emoticon([':-)',  ':)'],             "/images/icons/smiley.png",         "smiling"));
+plugin.emoticons.push(new Talker.Emoticon([':-D'],                    "/images/icons/smiley-grin.png",    "grin"));
+plugin.emoticons.push(new Talker.Emoticon(['X(', 'X-('],              "/images/icons/smiley-mad.png",     "angry"));
+plugin.emoticons.push(new Talker.Emoticon([':(', ':-('],              "/images/icons/smiley-sad.png",     "sad"));
+plugin.emoticons.push(new Talker.Emoticon([';(', ';-('],              "/images/icons/smiley-cry.png",     "cry"));
+plugin.emoticons.push(new Talker.Emoticon(['B)', 'B-)', '8)', '8-)'], "/images/icons/smiley-cool.png",    "cool"));
+plugin.emoticons.push(new Talker.Emoticon([':S', ':-S'],              "/images/icons/smiley-confuse.png", "confused"));
+plugin.emoticons.push(new Talker.Emoticon([':O', ':-O'],              "/images/icons/smiley-eek.png",     "shocked"));
+plugin.emoticons.push(new Talker.Emoticon([':P', ':-P'],              "/images/icons/smiley-razz.png",    "razz"));
 
-      if (!Talker.emoticons){
-        Talker.emoticons = [];
-      }
-
-      Talker.emoticons.push(new Talker.Emoticon(['>:-)', '>:)'],            "/images/icons/smiley-evil.png",    "evil"));
-      Talker.emoticons.push(new Talker.Emoticon([':-))', ':))'],            "/images/icons/smiley-lol.png",     "laughing"));
-      Talker.emoticons.push(new Talker.Emoticon([':-)',  ':)'],             "/images/icons/smiley.png",         "smiling"));
-      Talker.emoticons.push(new Talker.Emoticon([':-D'],                    "/images/icons/smiley-grin.png",    "grin"));
-      Talker.emoticons.push(new Talker.Emoticon(['X(', 'X-('],              "/images/icons/smiley-mad.png",     "angry"));
-      Talker.emoticons.push(new Talker.Emoticon([':(', ':-('],              "/images/icons/smiley-sad.png",     "sad"));
-      Talker.emoticons.push(new Talker.Emoticon([';(', ';-('],              "/images/icons/smiley-cry.png",     "cry"));
-      Talker.emoticons.push(new Talker.Emoticon(['B)', 'B-)', '8)', '8-)'], "/images/icons/smiley-cool.png",    "cool"));
-      Talker.emoticons.push(new Talker.Emoticon([':S', ':-S'],              "/images/icons/smiley-confuse.png", "confused"));
-      Talker.emoticons.push(new Talker.Emoticon([':O', ':-O'],              "/images/icons/smiley-eek.png",     "shocked"));
-      Talker.emoticons.push(new Talker.Emoticon([':P', ':-P'],              "/images/icons/smiley-razz.png",    "razz"));
 eos
     p.save
       
