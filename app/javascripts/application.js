e43e12ab72eca1eb2ffd8d746df86482c6793a97
Gary Haran <gary.haran@gmail.com>
2009-11-14 23:38:25 -0500

    sidebar enhancements specific to Firefox.

--- app/javascripts/application.js
+++ app/javascripts/application.js
@@ -20,10 +20,32 @@ $(function() {
   $('#room_name').click(function() {
     $('#rooms').toggle();
     $(this).find('span.switch_rooms').toggleClass('hide_rooms').toggleClass('show_rooms');
-  })
+  });
   
-  if ($.browser.safari){
+  
+  // search input fixes for various browsers.
+  if ($.browser.safari) {
     $('#rooms_controller.show input.search').css('width', '100%')
+  }
+  
+  if ($.browser.mozilla) {
+    $('input[placeholder]').each(function(){
+      $(this).data('original_color', $(this).css('color'));
+      
+      if ($(this).val() == '' || $(this).val() == $(this).attr('placeholder')){
+        $(this).val($(this).attr('placeholder')).css('color', '#CCC');
+      }
+      
+      $(this).focus(function(){
+        $(this).css('color', $(this).data('original_color')).val($(this).val() == $(this).attr('placeholder') ? '' : $(this).val());
+      }).blur(function() {
+        if ($(this).val() == '' || $(this).val() == $(this).attr('placeholder')){
+          $(this).css('color', '#CCC').val($(this).attr('placeholder'));
+        } else {
+          $(this).css('color', $(this).attr('original_color'));
+        }
+      })
+    });
   }
   
 });
