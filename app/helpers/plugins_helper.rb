c2bff90654d366399bb859f3ccbe3b5b8d10dca8
Gary Haran <gary.haran@gmail.com>
2009-11-15 00:32:08 -0500

    loading logs with a defer.

--- app/helpers/plugins_helper.rb
+++ app/helpers/plugins_helper.rb
@@ -38,4 +38,17 @@ Talker.Plugin_#{plugin.id} = function(){ // #{plugin.name} by #{plugin.author.na
   def render_events(events)
    "$.each(#{escape_json @events.to_json}, function(){ Talker.Broadcaster.broadcastEvent(this); });"
   end
+  
+  def render_events_for_logs(events)
+"
+var talker_events = #{escape_json @events.to_json};
+
+var loading_interval = window.setInterval(function(){
+  Talker.Broadcaster.broadcastEvent(talker_events.shift())
+  if (talker_events.length == 0){
+    window.clearInterval(loading_interval);
+  }
+}, 0);
+    "
+  end
 end
