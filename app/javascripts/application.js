92ff250bd811ab3d7376b701e21f954a7c0f5074
macournoyer <macournoyer@gmail.com>
2009-11-16 19:43:18 -0500

    Remove a buncha dead js code

--- app/javascripts/application.js
+++ app/javascripts/application.js
@@ -9,30 +9,9 @@ $(function() {
     return false;
   });
 
-  window.setTimeout(function(){
-    $("#invite_talker_team").click(function(){
-      $('#invitees').val('gary@talkerapp.com\nmacournoyer@talkerapp.com\n' + $('#invitees').val());
-    });
-    $('#invitees').focus(); 
-  }, 10);
-  
-  
-  $('#room_name').click(function() {
-    $('#rooms').toggle();
-    $(this).find('span.switch_rooms').toggleClass('hide_rooms').toggleClass('show_rooms');
+  $("#invite_talker_team").click(function(){
+    $('#invitees').val('gary@talkerapp.com\nmacournoyer@talkerapp.com\n' + $('#invitees').val());
   });
+  window.setTimeout(function(){ $('#invitees').focus(); }, 10);
   
-  
-  // search input fixes for various browsers.
-  if ($.browser.safari) {
-    $('#rooms_controller.show input.search').css('width', '120px')
-  }
-  
-  $('#toggle_extras').click(function() {
-    var expanded_src = '/images/icons/toggle.png';
-    var normal_src = '/images/icons/toggle-expand.png';
-    
-    $('#toggle_extras').attr('src', $('#toggle_extras').attr('src') == expanded_src ? normal_src : expanded_src );
-    $('#log_links').slideToggle(100, 'swing');
-  })
 });
