42bfde749735a9ef79ae8fddf6c336697540c654
Gary Haran <gary.haran@gmail.com>
2009-11-18 20:19:18 -0500

    removed invite talker team from sidebar on invite page.

--- app/javascripts/application.js
+++ app/javascripts/application.js
@@ -9,9 +9,6 @@ $(function() {
     return false;
   });
 
-  $("#invite_talker_team").click(function(){
-    $('#invitees').val('gary@talkerapp.com\nmacournoyer@talkerapp.com\n' + $('#invitees').val());
-  });
   window.setTimeout(function(){ $('#invitees').focus(); }, 10);
   
 });
