aef46f8cc87bc88b5b46af579d3ead07d576be91
macournoyer <macournoyer@gmail.com>
2009-11-20 11:02:08 -0500

    Remove feed url normalization, validation didn't allow it anyways.

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -55,7 +55,7 @@ class Feed < ActiveRecord::Base
     options[:if_none_match] = etag if etag
     options[:http_authentication] = [user_name, password] if user_name.present?
     
-    feed = Feedzirra::Feed.fetch_and_parse(normalized_url, options)
+    feed = Feedzirra::Feed.fetch_and_parse(url, options)
     
     if feed == 304 # not modified
       return
@@ -88,12 +88,6 @@ class Feed < ActiveRecord::Base
       "#{entry.author}: #{title} #{url}",
       (truncated_content unless title == content)
     ].compact
-  end
-  
-  def normalized_url
-    url.to_s.
-        gsub("feed:http", "http").
-        gsub("feed://", "http://")
   end
   
   def sanitize(content)
