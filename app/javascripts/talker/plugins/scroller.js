24e687ef617162812f6793eb1afc37c2b6e7b0d5
Gary Haran <gary.haran@gmail.com>
2009-11-20 15:41:05 -0500

    added inheritance pattern and updater scroller to separate edge cases.

--- app/javascripts/talker/plugins/scroller.js
+++ app/javascripts/talker/plugins/scroller.js
@@ -1,96 +1,88 @@
-// FF cannot be smart enough to fire onscroll event in the right order.
+ // FF cannot be smart enough to fire onscroll event in the right order.
 // the good news is that FF does provide mousedown and mouseup events when you touch the scrollbars.
 // so for Safari we use the canScroll variable to determine wether or not to enabled/disabled the autoscrolling.
 // in FF we will disable autoscroll if scrolling occurs between mousedown and mouseup events... provided the user
 // hasn't scrolled to the bottom of the page.
-Talker.Scroller = function() {
-  var self = this;
-  self.scrollAmount = 500000;
-  self.js_initiated_scroll = false;
-  
-  self.scrollToBottom = function(force){
-    if (force){
-      self.forcing = true;
-      window.scrollBy(0, self.scrollAmount = 500000);
-      self.forcing = false;
-    }
-    if (self.scrollAmount){
-      self.js_initiated_scroll = false;
-      window.scrollBy(0, self.scrollAmount);
-      self.js_initiated_scroll = true;
-    }
-  }
-  
-  self.nudge = function(height) {
-    self.js_initiated_scroll = false;false;
-    window.scrollBy(0, height);
-    self.js_initiated_scroll = true;
-  }
-  
-  $(window).mousewheel(function(event, delta) {
-    if (delta > 0){
-      self.disableAutoScrolling();
-    } else if (self.atBottom()) {
-      self.enableAutoScrolling();
-    }
-  });
 
-  if ($.browser.mozilla) {
-    $(document).mousedown(function(e) {
-      self.disableAutoScrolling()
-    }).mouseup(function(e) {
-      if (self.atBottom()){
-        self.enableAutoScrolling();
+Talker.MainScroller = Class.extend({
+  init: function(){
+    var self = this;
+    
+    self.defaultScrollAmount = 50000;
+
+    self.scrollBy = function(height) {
+      window.scrollBy(0, height);
+    }
+
+    self.scrollToBottom = function() {
+      if (self.defaultScrollAmount !== 0){
+        self.scrollBy(self.defaultScrollAmount);
+      }
+    }
+
+    self.atBottom = function() {
+      return !($(window).height() - $(document).height() + $(window).scrollTop());
+    }
+
+    $(window).mousewheel(function(event, delta) {
+      if (delta > 0){
+        self.defaultScrollAmount = 0;
+      } else if (self.atBottom()) {
+        self.defaultScrollAmount = 50000;
       }
     });
-  } else if ($.browser.safari) {
-    $(window).scroll(function(e) {
-      if (self.js_initiated_scroll && self.atBottom()) {
-        self.enableAutoScrolling();
-      } else {
-        if (self.forcing){ return }
-        self.disableAutoScrolling();
-      }
-    })
-  }
-  
-  $(window).load(function() {
-    self.scrollToBottom(true);
-  });
-  
-  self.atBottom = function() {
-    return self.scrollPosition() >= 0;
-  }
-  
-  self.scrollPosition = function() {
-    return $(window).height() - $(document).height() + $(window).scrollTop()
-  }
-  
-  self.scrollInterval = window.setInterval(function(){
-    self.scrollToBottom();
-  }, 10);
-  
-  self.enableAutoScrolling = function(){
-    self.scrollAmount = 500000;
-  }
-  
-  self.disableAutoScrolling = function(){
-    self.scrollAmount = 0;
-  }
-  
-  self.onJoin =
-  self.onLeave =
-  self.onNoticeInsertion =
-  self.onMessageInsertion = function(event) {
-    var lastInsertionHeight = $('#log p').height();
-    if (self.scrollPosition() - lastInsertionHeight <= 0){
-      self.nudge(lastInsertionHeight + 80); // + 80 for PREs and Youtubes
+
+    self.onJoin =
+    self.onLeave =
+    self.onNoticeInsertion =
+    self.onMessageInsertion = function(event) {
+      self.scrollToBottom();
     }
-    self.scrollToBottom();
+
+    self.onLoaded = 
+    self.onMessageSent = function(event) {
+      self.scrollToBottom();
+    }
   }
-  
-  self.onLoaded = 
-  self.onMessageSent = function(event) {
-    self.scrollToBottom(true);
-  }
+});
+
+if ($.browser.safari) { // webkit akshully only browser to handle order of onscroll events properly.
+  Talker.Scroller = Talker.MainScroller.extend({
+    init: function() {
+      var self = this;
+      
+      self._super(false);
+      
+      $(window).scroll(function(e){
+        if (!self.scrollingWithJS){
+          self.defaultScrollAmount = self.atBottom() ? 50000 : 0 ;
+        }
+      });
+    },
+    scrollBy: function(height) {
+      var self = this;
+      
+      self.scrollingWithJS = true;
+      window.scrollBy(0, height);
+      self.scrollingWithJS = false;
+    }
+  });  
+} else { // gecko based browsers and others
+  // hack to determine wether or not we scroll from user or not uses mouse down/up to determiner it.
+  // this has one drawback.  If a user has the mouse down during a call to scroll they might not scroll
+  // will see if users report any issues with this.
+  Talker.Scroller = Talker.MainScroller.extend({
+    init: function() {
+      var self = this;
+      self._super(false);
+      
+      $(document).mousedown(function(e) {
+        self.defaultScrollAmount = 0;
+      }).mouseup(function(e) {
+        if (self.atBottom()){
+          self.defaultScrollAmount = 50000;
+        }
+      });
+    }
+  });
 }
