3fc87063d178b21030ac07b876479cf3f70a109e
macournoyer <macournoyer@gmail.com>
2009-11-24 14:15:29 -0500

    Fix formatting plugins causing message to be treated as a paste.

--- app/javascripts/talker/plugins/pending.js
+++ app/javascripts/talker/plugins/pending.js
@@ -2,8 +2,9 @@
 Talker.Pending = function() {
   var self = this;
   
-  self.onMessageInsertion =
-  self.onNoticeInsertion = function(talkerEvent) {
+  self.onMessageReceived = function(talkerEvent) {
+    // Tag pending events for being replaced with real event
+    // from server.
     if (talkerEvent.id == "pending") {
       var line = Talker.getLastRow().find("p:last")
       
@@ -13,19 +14,20 @@ Talker.Pending = function() {
       // If it's a paste, we show a "loading" gif.
       if (Talker.isPaste(talkerEvent)) {
         line.html($("<img/>").attr("src", "/images/loader.gif"));
+        return false;
       }
+      
+      return;
     }
-  };
-  
-  // Remove first pending message if we receive a message from the current user.
-  self.onMessageReceived = function(talkerEvent) {
-    if (talkerEvent.id == "pending") return;
     
     var pending = $("#log .pending:first");
     
+    // We received the "real" version of a message tagged as pending.
+    // Update first pending message if we receive a message from the current user.
     if (pending[0] && talkerEvent.user.id == Talker.currentUser.id) {
       if (Talker.isPaste(talkerEvent)) {
-        // Pending pastes are shown as a loading gif.
+        // Pending pastes are shown as a loading gif. So just remove, will be replaced later
+        // in the chain.
         pending.remove();
       } else {
         pending.removeClass("pending").
