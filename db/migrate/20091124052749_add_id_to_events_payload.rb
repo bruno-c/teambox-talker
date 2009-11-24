37c48cf69685d038617e07c671a9e131fb020fec
macournoyer <macournoyer@gmail.com>
2009-11-24 00:47:32 -0500

    Fix nil error in lastest migration.

--- db/migrate/20091124052749_add_id_to_events_payload.rb
+++ db/migrate/20091124052749_add_id_to_events_payload.rb
@@ -2,13 +2,14 @@ class AddIdToEventsPayload < ActiveRecord::Migration
   def self.up
     Event.find_each do |event|
       object = event.payload_object
-      if payload_object.nil?
+      if object.nil?
         puts "Event ##{event.id} as an invalid payload"
-      end
-      if payload_object && object["id"].nil?
-        object["id"] = event.uuid
-        event.payload_object = object
-        event.save(false)
+      else
+        if object["id"].nil?
+          object["id"] = event.uuid
+          event.payload_object = object
+          event.save(false)
+        end
       end
     end
   end
