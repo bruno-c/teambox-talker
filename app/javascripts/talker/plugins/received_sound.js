0fbdf3451b7d324d49c09e7038d09046308ee697
Gary Haran <gary.haran@gmail.com>
2009-11-30 22:50:41 -0500

    added audio.  Enable it by typing /togglesound

---
+++ app/javascripts/talker/plugins/received_sound.js
@@ -0,0 +1,31 @@
+Talker.ReceivedSound = function() {
+  var plugin = this;
+  
+  plugin.command = 'togglesound';
+  plugin.usage = '/togglesound';
+  
+  plugin.onCommand = function(talkerEvent) {
+    if (talkerEvent.command == 'togglesound') {
+      $.cookie('ReceivedSound', $.cookie('ReceivedSound') == 'false' ? 'true' : 'false');
+      alert($.cookie('ReceivedSound') == 'true' ? "Audio alerts are now enabled." : "Audio alerts are now disabled.");
+      $('#msgbox').val('');
+      return false;
+    }
+  }
+  
+  plugin.onLoaded = function() {
+    if ($.cookie('ReceivedSound') != 'true') {
+      $.cookie('ReceivedSound', 'false');
+    }
+    
+    $(document.body).append($('<audio/>').attr('src', '/javascripts/borealis/message_received.wav')); // preloads for faster play on first message.
+      
+    plugin.onMessageReceived = function(talkerEvent) {
+      if (talkerEvent.user.id != Talker.currentUser.id && $.cookie('ReceivedSound') == 'true') {
+        $(document.body).append(
+          $('<audio/>').attr('src', '/javascripts/borealis/message_received.wav').attr('autoplay', 'true').bind('ended', function(){ $(this).remove() })
+        );
+      }
+    }
+  }
+}
