ce009fd1586770ce38fba4bb978a256d4f06fadd
Gary Haran <gary.haran@gmail.com>
2009-11-30 23:01:43 -0500

    organized audio files in a cleaner way.

--- app/javascripts/talker/plugins/received_sound.js
+++ app/javascripts/talker/plugins/received_sound.js
@@ -5,7 +5,7 @@ Talker.ReceivedSound = function() {
   plugin.usage = '/togglesound';
   
   plugin.onCommand = function(talkerEvent) {
-    if (talkerEvent.command == 'togglesound') {
+    if (talkerEvent.command == plugin.command) {
       $.cookie('ReceivedSound', $.cookie('ReceivedSound') == 'false' ? 'true' : 'false');
       alert($.cookie('ReceivedSound') == 'true' ? "Audio alerts are now enabled." : "Audio alerts are now disabled.");
       $('#msgbox').val('');
@@ -18,12 +18,12 @@ Talker.ReceivedSound = function() {
       $.cookie('ReceivedSound', 'false');
     }
     
-    $(document.body).append($('<audio/>').attr('src', '/javascripts/borealis/message_received.wav')); // preloads for faster play on first message.
+    $(document.body).append($('<audio/>').attr('src', '/sounds/borealis/message_received.wav')); // preloads for faster play on first message.
       
     plugin.onMessageReceived = function(talkerEvent) {
       if (talkerEvent.user.id != Talker.currentUser.id && $.cookie('ReceivedSound') == 'true') {
         $(document.body).append(
-          $('<audio/>').attr('src', '/javascripts/borealis/message_received.wav').attr('autoplay', 'true').bind('ended', function(){ $(this).remove() })
+          $('<audio/>').attr('src', '/sounds/borealis/message_received.wav').attr('autoplay', 'true').bind('ended', function(){ $(this).remove() })
         );
       }
     }
