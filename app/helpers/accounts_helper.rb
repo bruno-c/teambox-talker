5f86f06e2d2111ccd412be100c7a4b9046232c32
macournoyer <macournoyer@gmail.com>
2009-12-11 21:28:19 -0500

    Show usage as meters.

--- app/helpers/accounts_helper.rb
+++ app/helpers/accounts_helper.rb
@@ -6,4 +6,10 @@ module AccountsHelper
   def up_down_grade(plan)
     plan > @account.plan ? "Upgrade" : "Downgrade"
   end
+  
+  def meter_tag(rate)
+    content_tag(:div,
+      content_tag(:span, "&nbsp;", :style => "width: #{number_to_percentage rate * 100.0}"),
+      :class => "meter")
+  end
 end
