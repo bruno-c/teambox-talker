f9b8012b8ca67ddd96da7473309af769ae4baf6c
Gary Haran <gary.haran@gmail.com>
2009-12-13 17:08:55 -0500

    making youtube video embeds prettier.  Inspired by the new msgbox.

--- app/javascripts/talker/plugins/youtube_formatter.js
+++ app/javascripts/talker/plugins/youtube_formatter.js
@@ -6,12 +6,13 @@ Talker.YoutubeFormatter = function() {
     var youtube_match  = event.content.match(youtube_expression);
     
     if (youtube_match){
+      var color = (event.user.id == Talker.currentUser.id ? '&color1=0xD8CCBC&color2=0xFAEEDE' : '&color1=0xBBCCC6&color2=0xDDEFF8');
       Talker.insertMessage(event,
         '<object width="425" height="355" style="z-index: 1;">'
-        + '<param name="movie" value="http://www.youtube.com/v/' + youtube_match[1] + '&fs=1"></param>'
+        + '<param name="movie" value="http://www.youtube.com/v/' + youtube_match[1] + '&egm=1&rel=0&fs=1' + color + '"></param>'
         + '<param name="allowFullScreen" value="true"></param>'
         + '<param name="wmode" value="transparent">'
-        + '<embed src="http://www.youtube.com/v/' + youtube_match[1] + '&fs=1"'
+        + '<embed src="http://www.youtube.com/v/' + youtube_match[1] + '&egm=1&rel=0&fs=1' + color + '"'
         + ' type="application/x-shockwave-flash"'
         + '  width="425" height="355" '
         + '  allowfullscreen="true" wmode="transparent"></embed>'
