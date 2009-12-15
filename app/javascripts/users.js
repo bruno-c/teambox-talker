59d4200c0e4793eaee85e29cff4171391bb6aabf
macournoyer <macournoyer@gmail.com>
2009-12-14 23:02:01 -0500

    Replicate multiple asset host logic to js for avatars.

--- app/javascripts/users.js
+++ app/javascripts/users.js
@@ -1,9 +1,23 @@
 // Pass timezone offset from the browser to backend using a magic cookie
 $.cookie("tzoffset", (new Date()).getTimezoneOffset());
 
+String.prototype.retardedHash = function() {
+  var start = 0;
+  for (var i = 0; i < this.length; i++){
+    start += this.charCodeAt(i);
+  }
+  return parseInt(start);
+}
+
+function assetHost(path) {
+  var domain = location.host.match(/^\w+\.(.*)$/)[1];
+  var id = path.retardedHash() % 4;
+  return "//assets" + id + "." + domain + path;
+}
+
 function avatarUrl(user, size) {
   size = size || 18;
-  return "/avatar/" + MD5(user.email || "") + ".jpg?s=" + size;
+  return assetHost("/avatar/" + MD5(user.email || "") + ".jpg?s=" + size);
 }
 
 $(function() {
