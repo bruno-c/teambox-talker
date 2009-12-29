a13fb3354f0c60698d5197bfb89ac8113b870d99
Gary Haran <garyharan@stephanie-dugass-macbook.local>
2009-12-29 11:47:29 -0500

    using latest API calls for view in context

--- app/javascripts/talker/plugins/logs/logs_view_in_context.js
+++ app/javascripts/talker/plugins/logs/logs_view_in_context.js
@@ -2,14 +2,10 @@ Talker.LogsViewInContext = function(){
   var self = this;
   
   self.onMessageInsertion = function(event){
-    console.info(event);
-    var last_row = Talker.getLastRow();
-    var last_div = $(last_row).find('div:last');
-    
     var room = event.room.id;
     var id = event.id;
     
-    last_div.prepend(
+    Talker.getLastInsertion().prepend(
       $("<a/>").addClass('log')
         .attr("href", "/rooms/" + room + "/logs/" + FormatHelper.getUrlDate(event.time) + "#event_" + id)
         .text("View in context")
