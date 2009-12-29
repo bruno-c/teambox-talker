e9604a6a7bb63db658cb0f1ae4464311fb33b5ad
Gary Haran <garyharan@stephanie-dugass-macbook.local>
2009-12-29 11:41:41 -0500

    fixed view in context for logs

--- app/javascripts/talker/plugins/logs/logs_view_in_context.js
+++ app/javascripts/talker/plugins/logs/logs_view_in_context.js
@@ -2,13 +2,14 @@ Talker.LogsViewInContext = function(){
   var self = this;
   
   self.onMessageInsertion = function(event){
+    console.info(event);
     var last_row = Talker.getLastRow();
-    var last_p = $(last_row).find('p:last');
+    var last_div = $(last_row).find('div:last');
     
     var room = event.room.id;
     var id = event.id;
     
-    last_p.prepend(
+    last_div.prepend(
       $("<a/>").addClass('log')
         .attr("href", "/rooms/" + room + "/logs/" + FormatHelper.getUrlDate(event.time) + "#event_" + id)
         .text("View in context")
