5af5999ba9a05ee6df2704605566e1e8b0eb9eea
macournoyer <macournoyer@gmail.com>
2009-12-30 16:17:19 -0500

    Page cache everything in SiteController

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,4 +1,6 @@
 class SiteController < ApplicationController
+  caches_page :home, :pricing, :about, :privacy_policy, :service_policy, :terms_and_conditions
+  
   before_filter :dialog_layout, :only => [:privacy_policy, :service_policy, :terms_and_conditions]
   
   private
