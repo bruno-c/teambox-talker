f3ad032d804067396765a246ca50ec8d3525b8b7
macournoyer <macournoyer@gmail.com>
2009-12-29 22:09:49 -0500

    Use dialog layout for legal pages.

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,2 +1,8 @@
 class SiteController < ApplicationController
+  before_filter :dialog_layout, :only => [:privacy_policy, :service_policy, :terms_and_conditions]
+  
+  private
+    def dialog_layout
+      render :layout => "dialog"
+    end
 end
