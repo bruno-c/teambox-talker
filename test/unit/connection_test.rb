3e25c2d82df3649f5d157b2523e6f360bada78c1
macournoyer <macournoyer@gmail.com>
2010-01-15 16:15:21 -0500

    Add some tests to Connection model.

--- test/unit/connection_test.rb
+++ test/unit/connection_test.rb
@@ -1,7 +1,15 @@
 require File.dirname(__FILE__) + "/../test_helper"
 
 class ConnectionTest < ActiveSupport::TestCase
-  def test_creation
-    flunk "TODO"
+  def test_account_on_room
+    assert_equal rooms(:main).account, Connection.new(:channel => rooms(:main)).account
+  end
+
+  def test_account_on_paste
+    assert_equal pastes(:poem).room.account, Connection.new(:channel => pastes(:poem)).account
+  end
+
+  def test_account_on_nil
+    assert_equal nil, Connection.new.account
   end
 end
