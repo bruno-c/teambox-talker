71c64be4892ce2f43a2f087ba9db13f58da6689a
Gary Haran <garyharan@stephanie-dugass-macbook.local>
2010-01-20 18:18:29 -0500

    added more user friendly error handling.

--- app/javascripts/talker/broadcaster.js
+++ app/javascripts/talker/broadcaster.js
@@ -7,14 +7,26 @@ Talker.Broadcaster = {
   
   // Notify all subscribers about an event
   broadcast: function(eventName, eventData) {
-    var eventHandlerString = "on" + eventName;
-    for (var i = 0, len = this.plugins.length; i < len; i++){
-      var fn = this.plugins[i][eventHandlerString];
-      if (fn && fn(eventData) === false){
-        return false;
-      }
-    };
-    return true;
+    try{
+      var eventHandlerString = "on" + eventName;
+      for (var i = 0, len = this.plugins.length; i < len; i++){
+        var fn = this.plugins[i][eventHandlerString];
+        if (fn && fn(eventData) === false){
+          return false;
+        }
+      };
+      return true;      
+    }catch(e){
+      var error = '<b>Plugin Error (on' + eventName + '):</b><br/><br/>'
+        + e 
+        + '<br/><br/>Try disabling your plugins.  One of them is playing tricks on you!'
+        + "<br/><br/>Contact help@talkerapp.com if you need a hand with anything.";
+      
+      window.setTimeout(function(){
+        $.facebox(error);
+      }, 1000);
+      window.console && window.console.error && window.console.error(error.replace('<br/>', '\n'))
+    }
   },
   
   broadcastEvent: function(event) {
