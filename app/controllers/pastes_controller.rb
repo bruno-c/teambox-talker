3114586a8c6a2c0439e86e19077a44bfa31604f9
macournoyer <macournoyer@gmail.com>
2010-02-03 08:19:13 -0500

    Open paste to everyone. Remove "back to room" link on paste.

--- app/controllers/pastes_controller.rb
+++ app/controllers/pastes_controller.rb
@@ -6,8 +6,7 @@ class PastesController < ApplicationController
   def show
     @paste = Paste.find(params[:id])
     @full = (current_account.full? || @paste.full?) && !connected?(@paste)
-    # TODO                 v remove last part once feature out of beta
-    @can_connect = !@full && current_account.beta_tester
+    @can_connect = !@full
     
     if @paste.room && !current_user.permission?(@paste.room)
       raise ActiveRecord::RecordNotFound, "paste #{params[:id]} not found"
