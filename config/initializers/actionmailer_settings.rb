b41da491092653a895733d6f834d636e374b0a48
Josep Jaume <josepjaume@codegram.com>
2010-11-16 16:51:35 +0100

    Actionmailer settings moved to production

--- config/initializers/actionmailer_settings.rb
+++
@@ -1,9 +0,0 @@
-ActionMailer::Base.delivery_method = :smtp
-ActionMailer::Base.smtp_settings = {
-  :domain => 'app.teambox.com',
-  :address => 'smtp.sendgrid.net',
-  :port => 25,
-  :authentication => :plain,
-  :user_name => 'notifications@teamboxapp.com',
-  :password => 'kickME55'
-}
