81e30bea0868547b6dee6372af7e938c61838717
Josep Jaume <josepjaume@codegram.com>
2010-11-16 18:54:52 +0100

    All gems migrated to Gemfile. Now tests need to pass...

--- config/environment.rb
+++ config/environment.rb
@@ -7,12 +7,5 @@ Rails::Initializer.run do |config|
   config.load_paths += %W( #{RAILS_ROOT}/app/mailers
                            #{RAILS_ROOT}/app/jobs )
 
-  config.gem "tmm1-amqp", :lib => "amqp", :source => "http://gems.github.com"
-  config.gem "feedzirra", :version => '0.0.19'
-  config.gem "nokogiri"
-  config.gem "aws-s3", :lib => "aws/s3"
-  config.gem "uuid"
-  config.gem "brianmario-yajl-ruby", :lib => "yajl", :source => "http://gems.github.com"
-
   config.time_zone = 'UTC'
 end
