75826029a982db4ba8b297c92077716ead32a336
Josep M. Bach <josep.m.bach@gmail.com>
2010-11-23 17:59:00 +0100

    Further conversion from units to (passing) specs

--- spec/models/paste_spec.rb
+++ spec/models/paste_spec.rb
@@ -1,9 +1,9 @@
-require File.dirname(__FILE__) + "/../test_helper"
+require File.dirname(__FILE__) + "/../spec_helper"
 
-describe "Paste", ActiveSupport::TestCase do
+describe Paste do
   it "permalink is set" do
-    paste = create_paste
-    paste.id.should.not == nil
+    paste = Factory(:paste)
+    paste.id.should_not be_nil
   end
   
   it "truncate" do
@@ -18,6 +18,6 @@ describe "Paste", ActiveSupport::TestCase do
       paste = p
     end
     content.should == "ohaie\nthere"
-    paste.should.not == nil
+    paste.should_not be_nil
   end
 end
