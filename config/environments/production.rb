4501d3ae7f775576d886a7e3dc76fa1505502717
Josep M. Bach <josep.m.bach@gmail.com>
2010-11-23 11:58:41 +0100

    Removed ActionMailer SMTP config and changed chef recipe

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,13 +26,3 @@ config.action_controller.asset_host = "assets%d.talkerapp.com"
 
 # Enable threaded mode
 # config.threadsafe!
-
-ActionMailer::Base.delivery_method = :smtp
-ActionMailer::Base.smtp_settings = {
-  :domain => 'app.teambox.com',
-  :address => 'smtp.sendgrid.net',
-  :port => 25,
-  :authentication => :plain,
-  :user_name => 'notifications@teamboxapp.com',
-  :password => 'kickME55'
-}
