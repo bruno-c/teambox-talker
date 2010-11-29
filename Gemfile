616acd5c26241869aff8c945acb95800832ef5bf
Josep M. Bach <josep.m.bach@gmail.com>
2010-11-29 13:30:45 +0100

    Environment conflicts

---
+++ Gemfile
@@ -0,0 +1,28 @@
+source 'http://rubygems.org'
+source 'http://gems.github.com'
+
+gem "rails", "2.3.2"
+gem "haml"
+gem "compass"
+gem "sprockets"
+gem "eventmachine"
+gem "mysqlplus"
+gem "em-http-request"
+gem "tmm1-amqp", :require => "amqp"
+gem "feedzirra", '0.0.19'
+gem "shuber-attr_encrypted", :require => "attr_encrypted"
+gem "aws-s3", :require => "aws/s3"
+gem "uuid"
+gem "brianmario-yajl-ruby", :require => "yajl"
+gem "spreedly", :require => false
+
+group :development do
+  gem "thin"
+end
+
+group :test do
+  gem "nokogiri"
+  gem "mocha"
+  gem "rspec", "<2.0.0"
+  gem "rspec-rails", "<2.0.0"
+end
--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
+#RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -9,19 +9,6 @@ require File.join(File.dirname(__FILE__), 'boot')
 Rails::Initializer.run do |config|
   config.load_paths += %W( #{RAILS_ROOT}/app/mailers
                            #{RAILS_ROOT}/app/jobs )
-
-  config.gem "haml"
-  config.gem "chriseppstein-compass", :lib => "compass", :source => "http://gems.github.com/"
-  config.gem "tmm1-amqp", :lib => "amqp", :source => "http://gems.github.com"
-  config.gem "feedzirra", :version => '0.0.19'
-  config.gem "nokogiri"
-  config.gem "shuber-attr_encrypted", :lib => "attr_encrypted", :source => "http://gems.github.com"
-  config.gem "aws-s3", :lib => "aws/s3"
-  config.gem "uuid"
-  config.gem "brianmario-yajl-ruby", :lib => "yajl", :source => "http://gems.github.com"
-  config.gem "spreedly", :lib => false
-  config.gem "jammit"
-  config.gem "closure-compiler"
 
   config.time_zone = 'UTC'
 end
