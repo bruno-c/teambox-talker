f70db084a951c74d2dfb30c24ab805bd6d1fa189
Josep Jaume <josepjaume@gmail.com>
2010-12-03 12:03:34 +0100

    Updated cap task to ensure generating css and js

--- lib/recipes/assets.rb
+++ lib/recipes/assets.rb
@@ -1,4 +1,6 @@
 before "deploy:symlink", :roles => :app do
+  run "cd #{release_path}; RAILS_ENV=production bundle exec compass compile"
   run "cd #{release_path}; rake RAILS_ENV=production sprockets:generate"
+  run "cd #{release_path}; RAILS_ENV=production bundle exec jammit"
   run "cd #{release_path}; rake RAILS_ENV=production jsmin"
 end
