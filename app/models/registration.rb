9c35513a9c45ec1c88f3dd43204844011d392831
Josep Jaume <josepjaume@gmail.com>
2011-03-01 11:17:36 +0100

    Add some restrictions to the registration model

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -3,6 +3,8 @@ class Registration < ActiveRecord::Base
   belongs_to :user
   belongs_to :account
 
+  validates_uniqueness_of :user_id, :scope => [:account_id]
+
   # named_scope :registered, :conditions => { :guest => false }
   # named_scope :guests, :conditions => { :guest => true }
 
