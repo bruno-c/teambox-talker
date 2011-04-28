1acccf22290811e5f02ff51393def01c76fef734
Charles Barbier <unixcharles@gmail.com>
2011-04-27 23:33:55 -0700

    Remove monitor mailer spec

--- spec/models/mailers/monitor_mailer_spec.rb
+++
@@ -1,9 +0,0 @@
-require File.dirname(__FILE__) + "/../../spec_helper"
-
-describe MonitorMailer do
-  it "notifies signups" do
-    mail = MonitorMailer.create_signup(Factory(:account), Factory(:admin_user)).encoded
-    mail.should match "From: Talker Notifier <notifier@talkerapp.com>"
-    mail.should match "To: info@talkerapp.com"
-  end
-end
