1b266dbf8ea2c3b110d511f51375c28fad6fba51
Jordi Romero <jordi@jrom.net>
2011-04-27 19:18:17 -0700

    not loading payment gw config

--- config/initializers/spreedly.rb
+++ config/initializers/spreedly.rb
@@ -1,11 +1,1 @@
-SPREEDLY_CONFIG = YAML.load_file("#{RAILS_ROOT}/config/spreedly.yml")
-
-config = SPREEDLY_CONFIG[RAILS_ENV].symbolize_keys
-
-if config[:mock]
-  require "spreedly/mock"
-else
-  require "spreedly"
-end
-
-Spreedly.configure config[:site], config[:token]
+require "spreedly/mock"
